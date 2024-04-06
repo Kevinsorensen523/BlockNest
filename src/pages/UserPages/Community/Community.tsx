e345323e5b4372cc6117068de1edd45204a5e9a1
Kevinsorensen523 <94046712+Kevinsorensen523@users.noreply.github.com>
2024-04-06 12:17:19 +0700

    Add Comunity (Belum Kelar)

--- src/pages/UserPages/Community/Community.tsx
+++ src/pages/UserPages/Community/Community.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../../../Global.css";
 import {
+  IonButton,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonItem,
   IonLabel,
+  IonList,
   IonPage,
+  IonRow,
+  IonSearchbar,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
@@ -12,12 +19,48 @@ import SideMenu from "../../../components/SideMenu";
 import Header from "../../../components/Header";
 
 const Community: React.FC = () => {
+  const data = [
+    "Bitcoin",
+    "Ethereum",
+    "Solana",
+    "Doge",
+    "Manta",
+    "Ondo",
+    "Solidity",
+  ];
+  let [results, setResults] = useState([...data]);
+
+  const handleSearchChange = (event: CustomEvent) => {
+    const query = event.detail.value!.toLowerCase();
+    setResults(data.filter((d) => d.toLowerCase().includes(query)));
+  };
+
   return (
     <>
       <SideMenu />
       <IonPage id="main-content">
         <Header />
-        <IonLabel>Ini Komunitas</IonLabel>
+        <IonContent>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12">
+                <IonTitle className="ion-text-center mb-4">
+                  Join To Your Discussion Community
+                </IonTitle>
+                <IonList>
+                  {results.map((result, index) => (
+                    <IonRow>
+                      <IonButton key={index}>{result}</IonButton>
+                    </IonRow>
+                  ))}
+                </IonList>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonButton>Browse More</IonButton>
+            </IonRow>
+          </IonGrid>
+        </IonContent>
       </IonPage>
     </>
   );
---
+++ src/pages/UserPages/Profile/EditProfile.tsx
@@ -0,0 +1,10 @@
+import { IonGrid } from "@ionic/react";
+import React from "react";
+
+const EditProfile: React.FC = () => {
+  return (
+    <>
+      <IonGrid></IonGrid>
+    </>
+  );
+};
