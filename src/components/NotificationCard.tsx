8cede6fd007adfd22c5b78e31315fd04eeb6e41e
Kevinsorensen523 <94046712+Kevinsorensen523@users.noreply.github.com>
2024-05-09 16:00:36 +0700

    Add Notification UI

---
+++ src/components/NotificationCard.tsx
@@ -0,0 +1,78 @@
+import React, { useContext, useEffect, useState } from "react";
+import {
+  IonAvatar,
+  IonButton,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonPopover,
+  IonRow,
+  IonThumbnail,
+  IonToast,
+} from "@ionic/react";
+import {
+  heartOutline,
+  chatbubblesOutline,
+  ellipsisVertical,
+} from "ionicons/icons";
+import { close } from "ionicons/icons";
+import { AuthContext, PostObj, User } from "./context/AuthContext";
+import axios from "axios";
+import { useHistory } from "react-router";
+
+interface PostProps {
+  post: PostObj;
+  user: User;
+  onDelete?: (postId: number) => void;
+}
+
+const NotificationCard: React.FC<PostProps> = (props) => {
+  const history = useHistory();
+
+  return (
+    <>
+      <IonItem
+        style={{ "--background": "transparent", cursor: "pointer" }}
+        className="pt-1"
+      >
+        <IonAvatar slot="start">
+          <img
+            alt="Profile Picture"
+            src={`http://localhost:8000/${props.user.profile_pic}`}
+            className="w-10 h-10 ml-4"
+            onClick={() => history.push(`/people/${props.user.username}`)}
+          />
+        </IonAvatar>
+        <IonCol>
+          <IonCardHeader>
+            <IonCardTitle
+              onClick={() => history.push(`/people/${props.user.username}`)}
+              className="font-inknut text-white"
+            >
+              {props.user.username}
+            </IonCardTitle>
+            <IonCardSubtitle className="font-inknut text-white">
+              Liked Your Post
+            </IonCardSubtitle>
+          </IonCardHeader>
+        </IonCol>
+        <IonThumbnail>
+          <IonImg
+            alt="Post Image"
+            src={`http://localhost:8000/${props.post.image}`}
+          />
+        </IonThumbnail>
+      </IonItem>
+    </>
+  );
+};
+
+export default NotificationCard;
--- src/pages/UserPages/Notification/Notification.tsx
+++ src/pages/UserPages/Notification/Notification.tsx
@@ -1,5 +1,5 @@
 // Notification.tsx
-import React from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import {
   IonContent,
   IonPage,
@@ -19,84 +19,49 @@ import {
 import { chatbubblesOutline, heartOutline } from "ionicons/icons";
 import SideMenu from "../../../components/SideMenu";
 import Header from "../../../components/Header";
+import axios, { AxiosResponse } from "axios";
+import { PostObj } from "../../../components/context/AuthContext";
+import PostCard from "../../../components/PostCard";
+
+const NotificationCard = lazy(
+  () => import("../../../components/NotificationCard")
+);
 
 const Notification: React.FC = () => {
+  const url = "http://localhost:8000/home_page_posts.php";
+  const [data, setData] = useState<AxiosResponse>();
+  const [posts, setPosts] = useState<Array<PostObj>>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios.get(url).then((res) => {
+      setData(res);
+      console.log(res.data);
+      setPosts(res.data.post);
+    });
+  };
+
   return (
     <>
       <SideMenu />
       <IonPage id="main-content">
         <Header />
         <IonContent fullscreen>
-          <IonGrid>
-            <IonRow>
-              <IonCol size="12">
-                <Post />
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol size="12">
-                <Post />
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol size="12">
-                <Post />
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol size="12">
-                <Post />
-              </IonCol>
-            </IonRow>
-          </IonGrid>
+          <Suspense fallback={<div>Loading posts...</div>}>
+            <IonGrid>
+              {posts.map((post, index) => (
+                <NotificationCard key={index} post={post} user={post.user} />
+                // Comment Your Post
+                // Liked Your Post
+                // Mention You in A Post
+              ))}
+            </IonGrid>
+          </Suspense>
         </IonContent>
       </IonPage>
-    </>
-  );
-};
-
-const Post: React.FC = () => {
-  return (
-    <>
-      <IonItem style={{ "--background": "transparent" }} className="pt-1">
-        <IonAvatar slot="start">
-          <img
-            alt="Silhouette of a person's head"
-            src="https://ionicframework.com/docs/img/demos/avatar.svg"
-          />
-        </IonAvatar>
-        <IonCol>
-          <IonCardHeader>
-            <IonCardTitle>Raphael Hutapea</IonCardTitle>
-            <IonCardSubtitle>@hutapea</IonCardSubtitle>
-          </IonCardHeader>
-        </IonCol>
-      </IonItem>
-      <IonRow className="ml-24 pr-24 grid">
-        <IonLabel>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          consectetur purus ipsum, in dapibus velit sagittis in. Sed sodales, mi
-          accumsan finibus porttitor, est leo maximus velit, et vestibulum odio
-          augue sed risus. Pellentesque tempus lorem ac ultrices laoreet. Nulla
-          id eleifend mi, sit amet suscipit nisi. Mauris sagittis vehicula
-          libero quis lacinia. Maecenas rutrum tincidunt arcu non tincidunt.
-          Morbi sed elit non elit pulvinar molestie.
-        </IonLabel>
-        <IonThumbnail className="mt-4 w-full max-w-48 h-auto">
-          <img
-            alt="Silhouette of mountains"
-            src="https://ionicframework.com/docs/img/demos/thumbnail.svg"
-          />
-        </IonThumbnail>
-        <IonRow className="ion-align-items-start ion-justify-content-start mt-4">
-          <IonButton fill="clear" color="danger">
-            <IonIcon slot="start" icon={heartOutline} />
-          </IonButton>
-          <IonButton fill="clear" color="primary">
-            <IonIcon slot="start" icon={chatbubblesOutline} />
-          </IonButton>
-        </IonRow>
-      </IonRow>
     </>
   );
 };
